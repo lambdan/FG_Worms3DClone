ef6d6a2f126812506eae14d26461ecca41ec34db
David "djs <lambdan@users.noreply.github.com>
2022-09-18 01:19:36 +0200

    stop timer animation when time's out

--- Assets/HUDUpdater.cs
+++ Assets/HUDUpdater.cs
@@ -10,6 +10,7 @@ public class HUDUpdater : MonoBehaviour
     [SerializeField] private TMP_Text _currentPlayerText;
     [SerializeField] private TMP_Text _turnsPlayedText;
     [SerializeField] private Slider _turnTimeSlider;
+    [SerializeField] private Animation _turnTimeSliderAnimation;
 
     public void UpdateCurrentPlayerText(string newText)
     {
@@ -25,6 +26,14 @@ public class HUDUpdater : MonoBehaviour
     public void UpdateTurnSlider(float current)
     {
         _turnTimeSlider.value = current;
+        if (current < 0.1)
+        {
+            _turnTimeSliderAnimation.Stop();
+        }
+        else if (_turnTimeSliderAnimation.isPlaying == false)
+        {
+            _turnTimeSliderAnimation.Play();
+        }
     }
 
     public void SetTurnSliderMin(float min)
