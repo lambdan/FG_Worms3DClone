fc93d0ae2fad0757f7b3385acb1c2c8190e9421e
David "djs <lambdan@users.noreply.github.com>
2022-09-18 15:41:01 +0200

    add number to health bar

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,7 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Health _health;
     [SerializeField] private Slider _slider;
     [SerializeField] private Animation _heartAnimation;
+    [SerializeField] private TMP_Text _text;
 
     void Start()
     {
@@ -44,5 +46,6 @@ public class HealthBar : MonoBehaviour
     public void Refresh()
     {
         _slider.value = _health.GetHealth();
+        _text.text = _health.GetHealth().ToString();
     }
 }
